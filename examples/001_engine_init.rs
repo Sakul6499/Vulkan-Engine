254bbc0b9e96d881512a95a617c8129e719af086
Lukas Weber <dev@sakul-flee.de>
2023-07-30 03:26:48 +0200

    Moved example from Compute Engine Init

---
+++ examples/001_engine_init.rs
@@ -0,0 +1,21 @@
+// use vulkan_engine::{BaseEngine, ComputeEngine};
+// use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBufferUsage};
+
+// pub fn main() {
+//     let compute_engine = ComputeEngine::new();
+
+//     ComputeEngine::print_api_information(compute_engine.get_instance(), log::Level::Info);
+
+//     compute_engine.compute(&|engine: &ComputeEngine| {
+//         AutoCommandBufferBuilder::primary(
+//             engine.get_logical_device().get_device(),
+//             engine.get_logical_device().get_queue_family_index(),
+//             CommandBufferUsage::OneTimeSubmit,
+//         )
+//         .unwrap()
+//         .build()
+//         .unwrap()
+//     });
+
+//     compute_engine.kill();
+// }
